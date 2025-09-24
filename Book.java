5e7a8fae83e9e1c27954d73f9f2b2b18d4f73e9c
morish25 <23gloria65@gmail.com>
2025-09-24 14:12:12 +0900

    Update Book.java

--- Book.java
+++ Book.java
@@ -2,13 +2,13 @@ package ex11_01;
 
 public class Book extends TangibleAsset {
 	String isbn;
-	
+	int num;
 	public Book(String name, int price, String color, String isbn) {
 		super(name, price, color);
 		this.isbn = isbn;
 		
 	}
-	
+	System.out.println(num);
 	public String getIsbn() { return this.isbn; }
 }
 
